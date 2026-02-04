63a2dd828e082aafa6eeab9b71bb0e013fd07bc4
jamesProg100 <jamesmgraham990@gmail.com>
2026-02-04 18:34:05 +0000

    finishes dice app

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,4 +1,74 @@
 $(document).ready(function () {
   // Your code goes here
 
+  $("<div>")
+       .css({
+        height: 15,
+        width: 15,
+        backgroundColor: "black",
+        position: "absolute",
+        top: 50,
+        left: 50,
+
+      })
+  .appendTo("#die");
+
+
+  function makeDot(top, left, elementID) {
+    $("<div>")
+      .css({
+        height: 15,
+        width: 15,
+        backgroundColor: "black",
+        position: "absolute",
+        top: top,
+        left: left,
+
+      })
+    
+      .appendTo(elementID);
+  }
+
+ 
+  
+  function rollDie(dieID) {
+    $(dieID).empty();
+    var randomNum = Math.ceil(Math.random() * 6);
+    console.log(randomNum);
+
+    if (randomNum === 1) {
+      makeDot(50, 50, dieID); // middle middle
+    }  else if (randomNum === 2) {
+          makeDot(25, 25, dieID); // top left
+          makeDot(75, 75, dieID); // bottom right
+    }  else if (randomNum === 3) {
+          makeDot(25, 25, dieID); // top left
+          makeDot(75, 75, dieID); // bottom right
+          makeDot(50, 50, dieID); // middle middle
+    }  else if (randomNum === 4) {
+          makeDot(75, 75, dieID); // bottom right
+          makeDot(25, 25, dieID); // top left
+          makeDot(25, 75, dieID); // bottom left
+          makeDot(75, 25, dieID); // top right
+    }  else if (randomNum === 5) {
+          makeDot(50, 50, dieID); // middle middle
+          makeDot(75, 75, dieID); // bottom right
+          makeDot(25, 25, dieID); // top left
+          makeDot(25, 75, dieID); // bottom left
+          makeDot(75, 25, dieID); // top right
+    }
+      else if (randomNum === 6) { 
+          makeDot(25, 25, dieID); // top left
+          makeDot(25, 50, dieID); // middle left
+          makeDot(25, 75, dieID); // bottom left
+          makeDot(75, 25, dieID); // top right
+          makeDot(75, 50, dieID); // middle right
+          makeDot(75, 75, dieID); // bottom right
+    }
+
+  }
+  function handleClick(){
+    rollDie('#die');
+  }
+   $("#die").on("click", handleClick);
 });
